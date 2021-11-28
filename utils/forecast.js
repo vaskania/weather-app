c5a3d99b1225dc91ff16ef838db9f1092af55fc8
vaskaHome <vaskania26@gmail.com>
2021-11-29 00:54:08 +0400

    added s6 shorthand and destructuring

--- utils/forecast.js
+++ utils/forecast.js
@@ -3,15 +3,15 @@ const request = require('request');
 const forecast = (latitude, longitude, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=0514e774fed3bd8bd0cfd9e970f5d4b5&query=${latitude},${longitude}`;
 
-  request({ url: url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback('Unable to connect to weather service!', undefined);
-    } else if (response.body.error) {
+    } else if (body.error) {
       callback('unable to find location', undefined);
     } else {
-      const temp = response.body.current.temperature;
-      const feelsLike = response.body.current.feelslike;
-      const weatherDescription = response.body.current.weather_descriptions[0];
+      const temp = body.current.temperature;
+      const feelsLike = body.current.feelslike;
+      const weatherDescription = body.current.weather_descriptions[0];
 
       callback(
         undefined,
--- utils/geocode.js
+++ utils/geocode.js
@@ -4,18 +4,18 @@ const geocode = (address, callback) => {
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
     address
   )}.json?access_token=pk.eyJ1IjoidmFza2FuaWEiLCJhIjoiY2t3aXdnOHNpMWM5ODJ2cG04aWU4NDMxOSJ9.VdbcKDwp25gWd38OcIx3mA&limit=1`;
-  request({ url: url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback('Unable to connect to location services!', undefined);
-    } else if (response.body.message) {
-      callback(response.body.message, undefined);
-    } else if (response.body.features.length === 0) {
+    } else if (body.message) {
+      callback(body.message, undefined);
+    } else if (body.features.length === 0) {
       callback('Unable to find location. Try another search!', undefined);
     } else {
       callback(undefined, {
-        latitude: response.body.features[0].center[1],
-        longitude: response.body.features[0].center[0],
-        location: response.body.features[0].place_name,
+        latitude: body.features[0].center[1],
+        longitude: body.features[0].center[0],
+        location: body.features[0].place_name,
       });
     }
   });
