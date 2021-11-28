d4d64a3cf0acdbc2f833caab441e27e39f4d7585
vaskaHome <vaskania26@gmail.com>
2021-11-29 00:20:45 +0400

    added input city from cli

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-const address = ['42.275246', '42.7254'];
+const address = process.argv[2];
 
-forecast(address[0], address[1], (error, data) => {
-  console.log('Error', error);
-  console.log('Data', data);
-});
+if (!address) {
+  console.log('Please provide an address');
+} else {
+  geocode(address, (error, data) => {
+    if (error) {
+      return console.log(error);
+    }
 
-// geocode('Boston', (error, data) => {
-//   console.log('Error', error);
-//   console.log('Data', data);
-// });
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log(data.location);
+      console.log(forecastData);
+    });
+  });
+}
